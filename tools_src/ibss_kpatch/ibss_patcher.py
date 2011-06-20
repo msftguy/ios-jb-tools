c60b834c1d9e130cd0eb09d42e979cb15384268b
msftguy <msftguy@MyBook.local>
2011-06-20 08:30:57 +0400

    Fixed ibss_patcher to be more universal. Extra 20 bytes overhead in ibss_patchproc.bin (to locate kernel image addr from its entry point)

--- tools_src/ibss_kpatch/ibss_patcher.py
+++ tools_src/ibss_kpatch/ibss_patcher.py
@@ -39,16 +39,49 @@ def ibss_default_patches(origIbss, patchedIbss):
     ibss = mmap_file_rw(patchedIbss)
     return ibss
 
+def ibxx_load_addr(ibss):
+    # load addr+ some small value at 0x20, discard the lsb
+    load_addr = struct.unpack_from("<L", ibss, 0x20)[0] & ~0xff
+    print "iBSS/iBEC load addr is 0x%X" % load_addr
+    return load_addr
+    
+def ibxx_locate_bl(ibss):
+    kc_printf_arg = "kernelcache prepped at address 0x%"
+    printf_arg_loc = byte_search(ibss, kc_printf_arg)
+    if printf_arg_loc < 0:
+        raise Exception("Could not locate the printf argument (%s)!" % kc_printf_arg)
+    baseaddr = ibxx_load_addr(ibss)
+    printf_arg_xref = byte_search(ibss, struct.pack("<L", baseaddr + printf_arg_loc))
+    if printf_arg_xref < 0:
+        raise Exception("Could not locate any xrefs to '%s' string!" % kc_printf_arg)
+    # pattern search up until we can find the second BL instruction
+    # BL pattern is xx Fx xx F8+
+    max_search = 0x100
+    mask = 0xF800FF00
+    instr = 0xF800F000
+    bl_found = 0
+    for i in range(printf_arg_xref, printf_arg_xref - max_search, -2):
+        dw = struct.unpack_from("<L", ibss, i)[0]
+        if dw & mask == instr:
+            bl_found += 1
+            if bl_found == 2:
+                break
+    if bl_found != 2:
+        raise Exception("Could not find a printf call in the vicinity of string xref :-(")
+    print "printf call located at 0x%X (0x%X VA)" % (i, i + baseaddr)
+    return i
+        
+        
+
 def ibss_add_kpf(ibss, kpfFile):
     kpf = open(kpfFile, "rb").read()
     kpf_base = 0xFC
     ibss.seek(kpf_base)
     ibss.write(kpf)
-    #pattern match: bytes immediately after the BL to printf with kernel addr
-    after_call_loc = byte_search(ibss, "\x30\x46\x00\x21\x42\x46")
-    if after_call_loc < 0:
+    bl_loc = ibxx_locate_bl(ibss)
+    if bl_loc == 0:
         raise Exception("Could not locate the printf call; update the pattern!")
-    print "BL at %X" % (after_call_loc - 4)
+    after_call_loc = bl_loc + 4
     bl_range = (kpf_base - after_call_loc) / 2
     bitmask_11 = ((1 << 11) - 1)
     imm10 = bl_range >> 11
@@ -63,18 +96,24 @@ def ibss_add_kpf(ibss, kpfFile):
     
     
 def ibss_add_kpatches(ibss, kpatches):
+    magic = 0xDEADB33F
     for i in range(0, len(ibss), 4):
         dw = struct.unpack_from("<L", ibss, i)[0]
-        if dw == 0xdeadf00d:
-            print "Magic found at %X" % i
+        if dw == magic:
+            print "Magic (0x%X) found at 0x%X" % (magic, i)
+            struct.pack_into("<L", ibss, i, 0)
             for p in kpatches:
+                # check we aren't overwriting anything first
+                orig = struct.unpack_from("<LL", ibss, i)
+                if (orig[0] | orig[1]) != 0:
+                    raise Exception("Too many patches, out of 'free' space at 0x%X" % i)
                 struct.pack_into("<LL", ibss, i, p, kpatches[p])
                 i += 8
             return
-    raise Exception("Magic not found in ibss!")
+    raise Exception("Magic (0x%X) not found in ibss, ibss_patchproc.bin might be out of date!" % magic)
 
-def byte_search(image, bytes):
-    for i in range(0, len(image) - len(bytes), 2):
+def byte_search(image, bytes, step=1):
+    for i in range(0, len(image) - len(bytes), step):
 	if image[i:i+len(bytes)] == bytes:
 		return i
     return -1
--- tools_src/ibss_kpatch/ibss_patchproc.s
+++ tools_src/ibss_kpatch/ibss_patchproc.s
@@ -1,18 +1,30 @@
 
-@constants -----------------------------------
+#@constants -----------------------------------
+#.pool
+.thumb
 .pool
-
 entry_point:
-	ADR	R2, data_start
-.loop:
-	LDR	R3, [R2]
-	CBZ	R3, .out
-	LDR	R0, [R2, #4]
-	STR	R0, [R1, R3]
-	ADD	R2, #8 
-	B	.loop
+	ldr r2, =0xFeedFace
+.searchloop:
+	sub r1, #1
+	bfc r1, #0, #12
+	ldr r0, [r1]
+	cmp r0, r2
+	bne .searchloop
+
+	adr	r2, data_start
+.patchloop:
+	ldr	r3, [r2]
+	cbz	r3, .out
+	ldr	r0, [r2, #4]
+	str	r0, [r1, r3]
+	add	r2, #8 
+	b	.patchloop
 .out:
-	BX	LR
+	bx	lr
+
+# put all constants here	
+	.ltorg
 data_start:
-	.word	0xDEADF00D
+	.word	0xDeadB33f
 .end
