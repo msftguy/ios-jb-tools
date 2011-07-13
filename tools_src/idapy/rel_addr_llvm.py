9eedd926d7d412b0db630fdfdd42c70cefc77825
unknown <msftguy@.(none)>
2011-07-14 03:36:42 +0400

    Added IDA script to fix mov/movt/add reg,pc addressing Added some more README files

---
+++ tools_src/idapy/rel_addr_llvm.py
@@ -0,0 +1,88 @@
+import idaapi
+import idautils
+
+import time
+import struct
+
+# Fixes IDA's bug that it doesn't understand
+#   MOV.W Rn, imm16
+#   MOVT.W Rn, imm16
+#   ADD Rn, PC
+# as a data ref and doesn't generate xrefs for that
+# relevant for iOS 5.0 Beta 3 CommCenterClassic binary and idk what else..
+#
+# Pretty slow - mostly because of DecodeInstruction() calls, but prints don't help either..
+# Takes about a minute to grok CommCenterClassic on a Core 2 Duo
+#
+# idaapi.add_dref() sometimes silently fails - just rerun the script to get more xrefs ;)
+
+def main():
+    ea = 0
+    numInst = 0
+    numAddRegPc = 0
+    numFixed = 0
+    t0 = time.clock()
+    # last MOV/MOVT inst targeting the register, key=register number
+    movVal = dict()
+    movtVal = dict()
+
+    refs = []
+    while True:
+        ea = NextHead(ea)
+        if ea == BADADDR:
+            break
+        if not idaapi.isCode(idaapi.getFlags(ea)):
+            continue
+        numInst += 1
+        # slow, is there any way to avoid it?..
+        i = DecodeInstruction(ea)
+        mnem = i.get_canon_mnem()
+
+        if i[0].type != 1: # only interested in register target operands
+            continue
+        target_reg = i[0].phrase
+        if mnem == 'ADD':
+            if i[1].type == 1 and i[1].phrase == 15:
+                numAddRegPc += 1
+
+                (val, mov_ea) = movVal.get(target_reg, (0, 0))
+                (val_t, movt_ea) = movtVal.get(target_reg, (0, 0))
+                if not mov_ea:
+                    # No preceding MOV, bailing..
+                    continue
+                numFixed += 1
+
+                target_addr = 0xffffFFFF & (ea + 4 + val + 0x10000 * val_t)
+                # could be THUMB proc..
+                if target_addr & 1 and idaapi.isCode(idaapi.getFlags(target_addr - 1)):
+                    target_name = idaapi.get_name(target_addr - 1, target_addr - 1)                    
+                else:
+                    target_name = idaapi.get_name(target_addr,target_addr)
+                refs.append((ea, target_addr, target_name))
+
+        if i[1].type == 5:
+            if mnem == 'MOV':
+                movVal[target_reg] = (i[1].value, ea)
+                movtVal[target_reg] = (0, 0) # since movw clears top bits anyway
+            elif mnem == 'MOVT':
+                movtVal[target_reg] = (i[1].value, ea)
+        else:
+            movVal[target_reg] = (0, 0)
+            movtVal[target_reg] = (0, 0)
+    print "%u instructions scanned in %f seconds" % (numInst, time.clock() - t0)
+
+    for (ea, target_addr, target_name) in refs:
+        if target_name and len(target_name) != 0:
+            idaapi.set_cmt(ea, "%s - 0x%X" % (target_name, ea + 4), False)
+        else:
+            idaapi.set_cmt(ea, "0x%X - 0x%X" % (target_addr, ea + 4), False)
+        idaapi.add_dref(ea, target_addr, dr_O)
+
+    if numAddRegPc == 0:
+        successRate = 100
+    else:
+        successRate = numFixed * 100.0 / numAddRegPc
+    print "%u 'ADD Reg, PC' found, %u fixed: %u%%"  % (numAddRegPc, numFixed, successRate)
+
+if __name__ == "__main__":
+    main()
