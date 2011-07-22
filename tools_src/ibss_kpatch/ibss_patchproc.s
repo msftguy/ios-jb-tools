42a8f29c507a4f328c9a9a45bce7917ae33f6332
msftguy <msftguy@MyBook.local>
2011-07-22 08:41:19 +0400

    Added armv6 payload

--- tools_src/ibss_kpatch/ibss_patchproc.s
+++ tools_src/ibss_kpatch/ibss_patchproc.s
@@ -4,10 +4,15 @@
 .thumb
 .pool
 entry_point:
-	ldr r2, =0xFeedFace
+	ldr	r2, =0xFeedFace
 .searchloop:
 	sub r1, #1
+.ifdef ARMV7
 	bfc r1, #0, #12
+.else
+        ldr r0, =0xFFFFf000
+        and r1, r0
+.endif
 	ldr r0, [r1]
 	cmp r0, r2
 	bne .searchloop
@@ -15,7 +20,12 @@ entry_point:
 	adr	r2, data_start
 .patchloop:
 	ldr	r3, [r2]
+.ifdef ARMV7
 	cbz	r3, .out
+.else
+        movs    r3, r3
+        beq     .out
+.endif
 	ldr	r0, [r2, #4]
 	str	r0, [r1, r3]
 	add	r2, #8 
